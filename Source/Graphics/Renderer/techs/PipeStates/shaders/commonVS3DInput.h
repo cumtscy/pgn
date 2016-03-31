8608385f55c139d66de43704d0ea455a85740ff4
wk <wangkan@loongstudio.com>
2016-03-31 19:07:39 +0800

    修改文件名。

--- Source/Graphics/Renderer/techs/PipeStates/shaders/commonVS3DInput.h
+++
@@ -1,9 +0,0 @@
-
-in vec3 position;
-in vec3 normal;
-in int numAssignedBones;
-in uvec4 boneIndices;
-in vec4 boneWeights;
-in uint materialIndex;
-in vec2 texCoord0;
-in vec2 texCoord1;
--- Source/Graphics/Renderer/techs/PipeStates/shaders/commonVSOutput.h
+++
@@ -1,4 +0,0 @@
-
-out lowp vec3 n;
-out vec2 tc0;
-out vec2 tc1;
